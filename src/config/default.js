66f0df3413f740dd568a45a307f7a539e97fb455
Ahmet <113196405+AhmetShbz@users.noreply.github.com>
2024-09-27 22:00:55 +0200

    Update default.js

--- src/config/default.js
+++ src/config/default.js
@@ -9,10 +9,10 @@ module.exports = {
         userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     },
     proxy: {
-        host: '94.74.159.134',
-        port: '49155',
-        username: 'sanalbaba0',
-        password: 'R88nDa9sFb'
+        host: ''**********',',
+        port: ''**********',',
+        username: '**********',
+        password: ''**********','
     },
     recaptcha: {
         provider: '2captcha',
